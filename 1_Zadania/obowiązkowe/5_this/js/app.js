8138c4cde2dee0cb5e4341a88c3de92a30024745
adrskw <48623654+adrskw@users.noreply.github.com>
2019-10-18 20:36:58 +0200

    dział 5. zadanie 1

--- 1_Zadania/obowiązkowe/5_this/js/app.js
+++ 1_Zadania/obowiązkowe/5_this/js/app.js
@@ -13,3 +13,15 @@ var car = {
 car.printCarinfo();
 car.drive(20);
 car.printCarinfo();
+
+// Zadanie 1
+car.overviews = ["23.11.2016", "21.11.2017", "20.11.2018"];
+car.addOverview = function(date) {
+	this.overviews.push(date);
+}
+car.printOverviews = function() {
+	console.log(this.overviews);
+}
+
+car.addOverview("18.11.2019");
+car.printOverviews();
