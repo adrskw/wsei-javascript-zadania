d3f8993b09caa3fafe656e8f8a1e80c444a4f696
adrskw <48623654+adrskw@users.noreply.github.com>
2019-10-18 19:36:27 +0200

    dział 4. zadanie 0a

---
+++ 1_Zadania/obowiązkowe/4_Obiekty/js/app.js
@@ -0,0 +1,13 @@
+// Zadanie 0a
+var country = {
+	capital: "Warszawa",
+	population: 40000000,
+	president: "Andrzej Duda",
+	primeMinisters: ["Jan Kowalski", "Janusz Nowak", "Edyta Polak"]
+}
+console.log("Stolica: " + country.capital);
+console.log("Populacja: " + country.population);
+console.log("Prezydent: " + country.president);
+console.log("Premierzy: " + country.primeMinisters);
+
+// Zadanie 0b
