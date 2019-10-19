defe7d086368a91215144baeada3aecf083880b7
adrskw <48623654+adrskw@users.noreply.github.com>
2019-10-19 20:05:27 +0200

    dział 8. zadanie 0

--- 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
+++ 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
@@ -3,4 +3,17 @@ document.addEventListener("DOMContentLoaded", function () {
     Poniżej napisz kod rozwiązujący zadania. Odpowiedzi oddzielaj komentarzami.
      */
 
+	// Zadanie 0
+	var titleClassElement = document.querySelector(".title");
+	
+	function getDataAnimation(element) {
+		var dataAnimation = element.dataset.animation;
+		
+		return dataAnimation;
+	}
+	
+	console.log(getDataAnimation(titleClassElement));
+	getDataAnimation(titleClassElement);
+	
+	
 });
