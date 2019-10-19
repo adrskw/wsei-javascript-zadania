3b05ae03da14cafd83083f2ac02f7a4ee76ec5da
adrskw <48623654+adrskw@users.noreply.github.com>
2019-10-19 17:36:59 +0200

    dział 6. zadanie 1

--- 1_Zadania/obowiązkowe/6_constructor_function/js/app.js
+++ 1_Zadania/obowiązkowe/6_constructor_function/js/app.js
@@ -30,3 +30,41 @@ bruceBasket.addProduct("rice", 10);
 bruceBasket.addProduct("grzyby mun",50);
 bruceBasket.addProduct("tofu",20);
 bruceBasket.showBasket();
+
+// Zadanie 1
+var Robot = function (name) {
+    this.name = name;
+    this.isFunctional = true;
+}
+
+Robot.prototype.sayHi = function (toWho) {
+    if (this.isFunctional === true) {
+        console.log("Robot " + this.name + " greets " + toWho);
+    }
+    else {
+        console.log("Robot " + this.name + " is broken");
+    }
+};
+
+Robot.prototype.changeName = function (newname) {
+    console.log("Robot " + this.name + " changes name to " + newname);
+    this.name = newname;
+};
+
+Robot.prototype.fixIt = function () {
+    this.isFunctional = true;
+    console.log("Robot " + this.name + " was fixed");
+};
+
+var johnRobot = new Robot("John");
+var williamRobot = new Robot("William");
+var kateRobot = new Robot("Kate");
+var joshRobot = new Robot("Josh");
+
+joshRobot.sayHi("William");
+kateRobot.changeName("Tina");
+kateRobot.sayHi("Josh");
+williamRobot.isFunctional = false;
+williamRobot.sayHi("Tina");
+williamRobot.fixIt();
+williamRobot.sayHi("Tina");
