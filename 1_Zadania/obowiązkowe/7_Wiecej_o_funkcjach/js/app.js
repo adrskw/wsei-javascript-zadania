56eafb456ac15b1b843516d1826324494a22e187
adrskw <48623654+adrskw@users.noreply.github.com>
2019-10-19 19:11:38 +0200

    dział 7. zadanie 2

--- 1_Zadania/obowiązkowe/7_Wiecej_o_funkcjach/js/app.js
+++ 1_Zadania/obowiązkowe/7_Wiecej_o_funkcjach/js/app.js
@@ -44,3 +44,36 @@ function sortArray() {
 
 // wywołanie funkcji "sortArray"
 sortArray();
+
+// Zadanie 2
+function callOtherFunction(nameOfFunction) {
+    console.log("Cześć jestem funkcją o nazwie 'callOtherFunction' i wywołuję funkcję, którą ktoś mi wrzucił jako parametr");
+
+    var randomNumber1 = Math.random() * 20;
+    var randomNumber2 = Math.random() * 10;
+    nameOfFunction(randomNumber1, randomNumber2);
+}
+
+callOtherFunction(function (a, b) {
+    console.log("Pierwsza liczba:", a);
+    console.log("Druga liczba", b)
+    console.log("Wynik", a + b)
+});
+
+callOtherFunction(function (a, b) {
+    console.log("Pierwsza liczba:", a);
+    console.log("Druga liczba", b)
+    console.log("Wynik", a * b)
+});
+
+callOtherFunction(function (a, b) {
+	console.log("Pierwsza liczba:", a);
+    console.log("Druga liczba", b)
+    console.log("Mniejsza liczba ", Math.min(a, b));
+});
+
+callOtherFunction(function (a, b) {
+	console.log("Pierwsza liczba:", a);
+    console.log("Druga liczba", b)
+    console.log("Pierwsza liczba do potęgi drugiej ", Math.pow(a, b));
+});
