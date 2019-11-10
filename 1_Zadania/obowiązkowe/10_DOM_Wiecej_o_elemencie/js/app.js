6c21e972beb79d139e9a298157b678d7585883c5
adrskw <48623654+adrskw@users.noreply.github.com>
2019-11-10 15:24:23 +0100

    10. task 0

--- 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
+++ 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
@@ -2,5 +2,24 @@ document.addEventListener("DOMContentLoaded", function () {
     /*
     Poniżej napisz kod rozwiązujący zadania
      */
+	
+	// Zadanie 0
+	let elements = document.querySelectorAll(".ex5 li");
+	
+	for(var i = 0; i < elements.length; i++) {
+		if((i % 2) == 0) {
+			elements[i].style.backgroundColor = "green";
+		}
+		
+		if((i % 5) == 0) {
+			elements[i].className = "big";
+		}
 
+		if((i % 3) == 0) {
+			elements[i].children[0].style.textDecoration = "underline";
+		}
+		else {
+			elements[i].children[0].style.textDecoration = "none";
+		}
+	}
 });
