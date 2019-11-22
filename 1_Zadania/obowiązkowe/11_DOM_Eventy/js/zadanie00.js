dc611090af14af38000fc1664875aa52ba516b81
adrskw <48623654+adrskw@users.noreply.github.com>
2019-11-22 20:38:43 +0100

    11. task 0b

--- 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie00.js
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie00.js
@@ -1,5 +1,5 @@
 // Zadanie 0a
-var parentElements = document.getElementsByClassName("parent");
+/* var parentElements = document.getElementsByClassName("parent");
 
 for(var i = 0; i < parentElements.length; i++) {
 	parentElements[i].addEventListener("mouseenter", function(e) {
@@ -10,5 +10,29 @@ for(var i = 0; i < parentElements.length; i++) {
 	parentElements[i].addEventListener("mouseleave", function(e) {
 		let childElement = this.getElementsByClassName("children")[0];
 		childElement.style.display = "none";
+	});
+} 
+*/
+
+// Zadanie 0b
+var parentElements = document.getElementsByClassName("parent");
+
+for(var i = 0; i < parentElements.length; i++) {
+	parentElements[i].addEventListener("mouseenter", function(e) {			
+		function hideChildrenElement(){
+			let childElement = this.getElementsByClassName("children")[0];
+			childElement.style.display = "block";
+		}
+		
+		hideChildrenElement.call(this);
+	});
+
+	parentElements[i].addEventListener("mouseleave", function(e) {
+		function hideChildrenElement(){
+			let childElement = this.getElementsByClassName("children")[0];
+			childElement.style.display = "none";
+		}
+		
+		hideChildrenElement.call(this);
 	});
 } 
