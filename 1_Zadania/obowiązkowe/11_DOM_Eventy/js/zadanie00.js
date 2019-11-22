48b3f07c61e4ccf92d8c51e06277f07c5a731417
adrskw <48623654+adrskw@users.noreply.github.com>
2019-11-22 20:06:00 +0100

    11. task 0a

---
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie00.js
@@ -0,0 +1,14 @@
+// Zadanie 0a
+var parentElements = document.getElementsByClassName("parent");
+
+for(var i = 0; i < parentElements.length; i++) {
+	parentElements[i].addEventListener("mouseover", function(e) {
+		let childElement = this.getElementsByClassName("children")[0];
+		childElement.style.display = "block";
+	});
+
+	parentElements[i].addEventListener("mouseleave", function(e) {
+		let childElement = this.getElementsByClassName("children")[0];
+		childElement.style.display = "none";
+	});
+} 
