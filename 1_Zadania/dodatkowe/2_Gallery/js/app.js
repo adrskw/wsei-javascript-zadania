8d43f4a2683110c33a1ecb9800338f624b28169b
adrskw <48623654+adrskw@users.noreply.github.com>
2020-01-06 03:22:56 +0100

    extra tasks 2. task 2

---
+++ 1_Zadania/dodatkowe/2_Gallery/js/app.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Punkt 2
+    console.log("Dzia³a");
+    var list = document.querySelectorAll(".gallery ul li");
+    var bodyTag = document.body;
+
+    console.log(list);
+    console.log(bodyTag);
+
+    
+});
