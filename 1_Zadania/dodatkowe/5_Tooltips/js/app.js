4a1694e41b84fd48145093eec4efe383c697e847
adrskw <48623654+adrskw@users.noreply.github.com>
2020-01-06 23:52:00 +0100

    extra tasks 5. task 3

--- 1_Zadania/dodatkowe/5_Tooltips/js/app.js
+++ 1_Zadania/dodatkowe/5_Tooltips/js/app.js
@@ -8,4 +8,14 @@ document.addEventListener("DOMContentLoaded", function () {
     var tooltips = document.getElementsByClassName("tooltip");
 
     console.log(tooltips);
+
+    for (var i = 0; i < tooltips.length; i++) {
+        tooltips[i].addEventListener("mouseover", function () {
+            console.log("najechano");
+        });
+
+        tooltips[i].addEventListener("mouseout", function () {
+            console.log("wyjechano");
+        });
+    }
 });
