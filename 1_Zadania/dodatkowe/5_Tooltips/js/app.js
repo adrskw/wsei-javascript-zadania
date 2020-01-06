a36dd7b14dd82cabb034ba5f92ce06423aeb00f4
adrskw <48623654+adrskw@users.noreply.github.com>
2020-01-06 23:50:26 +0100

    extra tasks 5. task 1, 2

--- 1_Zadania/dodatkowe/5_Tooltips/js/app.js
+++ 1_Zadania/dodatkowe/5_Tooltips/js/app.js
@@ -1,3 +1,11 @@
 /*
  <span class="tooltipText">Text tooltipa</span>
  */
+document.addEventListener("DOMContentLoaded", function () {
+    // punkt 2
+    console.log("Dziala");
+
+    var tooltips = document.getElementsByClassName("tooltip");
+
+    console.log(tooltips);
+});
