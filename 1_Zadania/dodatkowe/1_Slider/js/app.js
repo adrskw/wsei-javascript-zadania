9333aa1956db7cc0cc100560dd927210cedaea20
adrskw <48623654+adrskw@users.noreply.github.com>
2020-01-06 02:53:55 +0100

    extra tasks 1. task 1, 2

---
+++ 1_Zadania/dodatkowe/1_Slider/js/app.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // punkt 2
+    console.log("Działa");
+
+    var prev = document.getElementById("prevPicture");
+    var next = document.getElementById("nextPicture");
+    var list = document.querySelectorAll(".slider ul li");
+    var imageIndex = 0;
+    console.log(prev);
+    console.log(next);
+    console.log(list);
+    console.log(imageIndex);
+
+
+});
