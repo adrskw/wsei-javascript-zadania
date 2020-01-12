f67db5d08da031e61a99cc7f4ef2d2da11a715ff
adrskw <48623654+adrskw@users.noreply.github.com>
2020-01-12 09:26:18 +0100

    extra tasks 11. task 1, 2

--- 1_Zadania/dodatkowe/11_Array_operations/js/app.js
+++ 1_Zadania/dodatkowe/11_Array_operations/js/app.js
@@ -5,9 +5,30 @@ document.addEventListener('DOMContentLoaded', function () {
     })
     console.log(result);
 
+    // Zadanie 0
     var cities = ["Kraków", "Olsztyn", "Szczecin", "Ostrów Wielkopolski"];
     var citiesCount = cities.map(function (city) {
         return city.length;
     });
     console.log(citiesCount);
+
+    // Zadanie 1
+    function randomize(param1, param2, callback) {
+        var randomNumber = Math.floor(Math.random() * param2) + param1;
+
+        if (callback instanceof Function) {
+            callback(randomNumber);
+        }
+    }
+
+    randomize(1, 6, function (number) {
+        console.log("wylosowana liczba: " + number)
+    });
+    randomize(1, 6, "test");
+
+    // Zadanie 2
+    var animals = ["cat", "shrimp", "giraffe"];
+    animals.forEach(function (animal) {
+        console.log(animal);
+    });
 })
